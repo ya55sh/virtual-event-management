ca9e7447eb7d3b2206ea9e5fa8f41f41f42eb9ac
ya55sh <yashbahuguna10@gmail.com>
2025-04-19 15:41:21 +0530

    -refactored app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const route = require("./routes/index.route");
+const route = require("./routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
